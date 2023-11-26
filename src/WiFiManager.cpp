aefa830a64aaea3d29d425bc9345af8da1473d45
Leon Latsch <leon.latsch@parshipgroup.com>
2023-11-26 15:01:18 +0100

    fix class method implementation

--- src/WiFiManager.cpp
+++ src/WiFiManager.cpp
@@ -4,56 +4,49 @@
 
 #include "WiFiManager.h"
 
+const char *PREFS_KEY_SSID = "SSID";
+const char *PREFS_KEY_PASSWORD = "PASS";
+
 const char *AP_SSID = "ESP32 Intercom Controller";
 
-class WiFiManager {
-    Preferences prefs;
+void print_connection(String ssid, IPAddress ip) {
+    Serial.print(ssid);
+    Serial.print(" | ");
+    Serial.println(ip);
+}
 
-    public:
-    WiFiManager(Preferences &_prefs) {
-        prefs = _prefs;
+void WiFiManager::setup_wifi_sta() {
+    Serial.println("Starting WiFi in STA mode");
+
+    String ssid = prefs.getString(PREFS_KEY_SSID);
+    String pass = prefs.getString(PREFS_KEY_PASSWORD);
+
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, pass);
+
+    // Wait for connection
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(100);
     }
 
-    void setup_wifi_sta() {
-        Serial.println("Starting WiFi in STA mode");
-
-        String ssid = prefs.getString(PREFS_KEY_SSID);
-        String pass = prefs.getString(PREFS_KEY_PASSWORD);
-
-        WiFi.mode(WIFI_STA);
-        WiFi.begin(ssid, pass);
-
-        // Wait for connection
-        while (WiFi.status() != WL_CONNECTED) {
-            delay(100);
-        }
-
-        if (WiFi.isConnected()) {
-            print_connection(ssid, WiFi.localIP());
-            blink(2);
-        }
+    if (WiFi.isConnected()) {
+        print_connection(ssid, WiFi.localIP());
+        blink(2);
     }
+}
 
-    void setup_wifi_ap() {
-        Serial.println("Starting WiFi in AP mode");
+void WiFiManager::setup_wifi_ap() {
+     Serial.println("Starting WiFi in AP mode");
 
-        // SSID, no password, channel 1, not hidden, 1 connection max
-        WiFi.softAP(AP_SSID, NULL, 1, 0, 1);
-        print_connection(AP_SSID, WiFi.softAPIP());
-        digitalWrite(LED_BLUE, HIGH);
-    }
+    // SSID, no password, channel 1, not hidden, 1 connection max
+    WiFi.softAP(AP_SSID, NULL, 1, 0, 1);
+    print_connection(AP_SSID, WiFi.softAPIP());
+    digitalWrite(LED_BLUE, HIGH);
+}
 
-    bool wifi_creds_configured() {
-        String ssid = prefs.getString(PREFS_KEY_SSID);
-        String pass = prefs.getString(PREFS_KEY_PASSWORD);
+bool WiFiManager::wifi_creds_configures() {
+    String ssid = prefs.getString(PREFS_KEY_SSID);
+    String pass = prefs.getString(PREFS_KEY_PASSWORD);
 
-        return !ssid.isEmpty() && !pass.isEmpty();
-    }
-    
-    protected:
-    void print_connection(String ssid, IPAddress ip) {
-        Serial.print(ssid);
-        Serial.print(" | ");
-        Serial.println(ip);
-    }
-};
+    return !ssid.isEmpty() && !pass.isEmpty();
+}
--- src/WiFiManager.h
+++ src/WiFiManager.h
@@ -1,19 +1,23 @@
-#ifndef WIFIMANAGER_H
-#define WIFIMANAGER_H
+#ifndef WiFiManager_H
+#define WiFiManager_H
 
 #include <Preferences.h>
 
-const char *PREFS_KEY_SSID = "SSID";
-const char *PREFS_KEY_PASSWORD = "PASS";
+extern const char *PREFS_KEY_SSID;
+extern const char *PREFS_KEY_PASSWORD;
 
 class WiFiManager;
 
 class WiFiManager {
+    private:
+        Preferences prefs;
     public:
-    WiFiManager(Preferences _prefs);
-    void setup_wifi_sta();
-    void setup_wifi_ap();
-    bool wifi_creds_configures();
+        WiFiManager(Preferences _prefs) {
+            prefs = _prefs;
+        }
+        void setup_wifi_sta();
+        void setup_wifi_ap();
+        bool wifi_creds_configures();
 };
 
 #endif
